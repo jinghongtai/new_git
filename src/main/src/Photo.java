b4246df0a041e14d5943240ea360f9274ff04966
jinghongtai <969568361@qq.com>
2018-08-13 10:11:29 +0800

    新增头像、删除班级

---
+++ src/main/src/Photo.java
@@ -0,0 +1,50 @@
+/**
+ * 版权归公司所有
+ * 项目名称： 应用支撑平台;
+ * 创建者    :  jinghongtai;
+ * 创建日期: 2018年08月13日 10:10;
+ *
+ * @version: 1.0
+ */
+public class Photo {
+
+    private String id;
+
+    private String cover;
+
+    private String name;
+
+    private String path;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
