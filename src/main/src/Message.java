307934554d18c2665ae2593498d9203688dbdc2f
jinghongtai <969568361@qq.com>
2018-08-13 10:22:25 +0800

    新增信息及订单信息表

---
+++ src/main/src/Message.java
@@ -0,0 +1,61 @@
+/**
+ * 版权归公司所有
+ * 项目名称： 应用支撑平台;
+ * 创建者    :  jinghongtai;
+ * 创建日期: 2018年08月13日 10:20;
+ *
+ * @version: 1.0
+ */
+public class Message {
+
+    private String id;
+
+    private String title;
+
+    private String content;
+
+    private int release;
+
+    private String createTime;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRelease() {
+        return release;
+    }
+
+    public void setRelease(int release) {
+        this.release = release;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ src/main/src/Order.java
@@ -0,0 +1,51 @@
+/**
+ * 版权归公司所有
+ * 项目名称： 应用支撑平台;
+ * 创建者    :  jinghongtai;
+ * 创建日期: 2018年08月13日 10:21;
+ *
+ * @version: 1.0
+ */
+public class Order {
+
+    private String id;
+
+    private String orderNo;
+
+    private String tradeNo;
+
+    private String createTime;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getTradeNo() {
+        return tradeNo;
+    }
+
+    public void setTradeNo(String tradeNo) {
+        this.tradeNo = tradeNo;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
