9b8ffab9a5e5680b136bd555ead6576ac1a0a3c5
jinghongtai <969568361@qq.com>
2018-08-13 09:32:44 +0800

    用户新增邮箱

--- src/main/src/SysUser.java
+++ src/main/src/SysUser.java
@@ -20,6 +20,15 @@ public class SysUser {
 
     private String zipcode;
 
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getZipcode() {
         return zipcode;
